1e30adf96e88946a95b40f40ee6366d02b80064e
Cindy Eude <eudecindy@gmail.com>
2026-06-03 16:20:15 +0200

    nouvelle-page-history-au-0306

--- db/migrate/20260603124053_add_score_to_analyses.rb
+++
@@ -1,5 +0,0 @@
-class AddScoreToAnalyses < ActiveRecord::Migration[8.1]
-  def change
-    add_column :analyses, :score, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_03_124053) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_02_103955) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -70,6 +70,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_03_124053) do
 
   create_table "messages", force: :cascade do |t|
     t.bigint "chat_id", null: false
+    t.text "content"
     t.datetime "created_at", null: false
     t.integer "role"
     t.datetime "updated_at", null: false
