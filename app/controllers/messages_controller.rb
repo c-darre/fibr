81c56d77c298037786a5538167441336bab49417
Benoit GILLIOT <166861063+benoit-korben@users.noreply.github.com>
2026-06-04 17:53:07 +0200

    moved to a multi LLM architecture

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,15 +1,36 @@
 class MessagesController < ApplicationController
   skip_before_action :authenticate_user!
 
-  def create
-    @analysis = Analysis.find(params[:analysis_id])
-    chat = @analysis.chat
+  # def create
+  #   @analysis = Analysis.find(params[:analysis_id])
+  #   chat = @analysis.chat #adapt
 
-    message = chat.messages.build(role: :user)
-    message.photos.attach(message_params[:photos]) if message_params[:photos].present?
-    message.save!
-    @analysis.processing!
-    AnalyzeGarmentJob.perform_later(@analysis.id)
+  #   message = chat.messages.build(role: :user)
+  #   message.photos.attach(message_params[:photos]) if message_params[:photos].present?
+  #   message.save!
+  #   @analysis.processing!
+  #   AnalyzeGarmentJob.perform_later(@analysis.id)
+
+  #   redirect_to analysis_path(@analysis)
+  # end
+
+  def create
+    @chat = Chat.find(params[:chat_id])
+    @analysis = @chat.analysis
+    message  = @chat.messages.build(role: :user)
+
+    case @chat.kind
+    when "analysis"
+      message.photos.attach(message_params[:photos])
+      message.save!
+      @analysis.processing!
+      AnalyzeGarmentJob.perform_later(@analysis.id)
+    when "discussion"
+      message.content = message_params[:content]
+      message.save!
+      DiscussionJob.perform_later(@chat.id)
+    when "questionnary"
+    end
 
     redirect_to analysis_path(@analysis)
   end
@@ -17,6 +38,6 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(photos: [])
+    params.require(:message).permit(:content, photos: [])
   end
 end
--- app/models/analysis.rb
+++ app/models/analysis.rb
@@ -1,7 +1,11 @@
 class Analysis < ApplicationRecord
   belongs_to :user, optional: true
 
-  has_one :chat, dependent: :destroy
+  has_many :chats, dependent: :destroy
+  has_one :analysis_chat,     -> { where(kind: :analysis)      }, class_name: "Chat"
+  has_one :discussion_chat,   -> { where(kind: :discussion)    }, class_name: "Chat"
+  has_one :photo_review_chat, -> { where(kind: :photo_review)  }, class_name: "Chat"
+  has_one :questionnary_chat, -> { where(kind: :questionnary)  }, class_name: "Chat"
   has_many :criteria, class_name: "Criterium", dependent: :destroy
 
   enum :status, { pending: 0, processing: 1, completed: 2, failed: 3 }, default: :pending
