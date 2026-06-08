ea04261d679263fac2e93989045eace6ad843a33
Alicia Vence <alizia.vence.art@gmail.com>
2026-06-08 09:24:04 +0200

    test

--- db/schema.rb
+++ db/schema.rb
@@ -45,7 +45,10 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_05_131004) do
   create_table "analyses", force: :cascade do |t|
     t.float "co2"
     t.datetime "created_at", null: false
+    t.jsonb "ecobalyse_fields"
+    t.string "garment_size"
     t.string "garment_type"
+    t.float "global_score"
     t.integer "score"
     t.integer "status"
     t.datetime "updated_at", null: false
