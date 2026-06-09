33431c8f9627d0518b6b9971e5ef2df25b47f414
cyprien darre <cyprien.darre@gmail.com>
2026-06-09 17:30:16 +0200

    feat: add custom fonts (Savoye & Optician Sans) and configure asset

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,6 @@ Rails.application.config.assets.version = "1.0"
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.precompile += %w(bootstrap.min.js popper.js)
+
+# Fonts 
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
